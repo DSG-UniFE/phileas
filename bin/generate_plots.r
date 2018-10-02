7498a823444dd086647113793a3c01a144665e84
Filippo Poltronieri <fpoltronieri@ihmc.us>
2018-10-02 16:52:38 +0200

    minor change in the R script

--- bin/generate_plots.r
+++ bin/generate_plots.r
@@ -14,7 +14,6 @@ vp <- ggplot(voi_data, aes(x=nr, y=OutputVoI, color=ContentType))
 vp + geom_smooth() + facet_wrap(~ContentType, nrow=4) + xlab("Time") +
         ylab("VoI") + theme_bw() + theme(legend.position="none", text = element_text(size=15)) 
 voict_fn = paste("voi_ct_",gsub("^a-z*", "", filename), ".png", sep="")
-
 # fix plot aspect ratio
 aspect_ratio <- 2.5
 height <- 7
@@ -24,7 +23,6 @@ ggsave(voict_fn,  height = 5 , width = 5 * aspect_ratio)
 vp + geom_smooth() + xlab("Time") +
         ylab("VoI") + theme_bw() + theme(text = element_text(size=15)) 
 voict_fn_all = paste("voi_ct_all_",gsub("[a-z _ .]", "", filename), ".png", sep="")
-
 
 # save plot with all services on the same graph
 ggsave(voict_fn_all, height = 5 , width = 5 * aspect_ratio)
